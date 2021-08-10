2fed0c9dcaba0f2f91d41d80f099ffdd92b21caf
peng00bo00 <pengbo_tongji@126.com>
2021-08-10 16:53:05 +0400

    update

--- assignments/hw5/src/denoiser.cpp
+++ assignments/hw5/src/denoiser.cpp
@@ -112,76 +112,154 @@ Buffer2D<Float3> Denoiser::Filter(const FrameInfo &frameInfo) {
     int width = frameInfo.m_beauty.m_width;
     Buffer2D<Float3> filteredImage = CreateBuffer2D<Float3>(width, height);
     int kernelRadius = 16;
-#pragma omp parallel for
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            // TODO: Joint bilateral filter
-            filteredImage(x, y) = frameInfo.m_beauty(x, y);
-            // continue;
+// #pragma omp parallel for
+//     for (int y = 0; y < height; y++) {
+//         for (int x = 0; x < width; x++) {
+//             // TODO: Joint bilateral filter
+//             filteredImage(x, y) = frameInfo.m_beauty(x, y);
+//             // continue;
 
-            // value and weight
-            Float3 value_sum(0.0);
-            float w, wp, wc, wn, wd;
-            float weight_sum= 0.0;
+//             // value and weight
+//             Float3 value_sum(0.0);
+//             float w, wp, wc, wn, wd;
+//             float weight_sum= 0.0;
 
-            // filter window
-            int x1 = x - kernelRadius;
-            int x2 = x + kernelRadius;
-            int y1 = y - kernelRadius;
-            int y2 = y + kernelRadius;
+//             // filter window
+//             int x1 = x - kernelRadius;
+//             int x2 = x + kernelRadius;
+//             int y1 = y - kernelRadius;
+//             int y2 = y + kernelRadius;
 
-            if (x1 < 0) x1 = 0;
-            if (x2 > width) x2 = width;
+//             if (x1 < 0) x1 = 0;
+//             if (x2 > width) x2 = width;
 
-            if (y1 < 0) y1 = 0;
-            if (y2 > height) y2 = height;
+//             if (y1 < 0) y1 = 0;
+//             if (y2 > height) y2 = height;
 
-            for (size_t i = x1; i < x2; ++i)
-            {
-                for (size_t j = y1; j < y2; ++j)
-                {
-                    // weights
-                    // position weight
-                    wp = (i-x)*(i-x) + (j-y)*(j-y);
-                    wp = wp / (2.0*m_sigmaCoord);
+//             for (size_t i = x1; i < x2; ++i)
+//             {
+//                 for (size_t j = y1; j < y2; ++j)
+//                 {
+//                     // weights
+//                     // position weight
+//                     wp = (i-x)*(i-x) + (j-y)*(j-y);
+//                     wp = wp / (2.0*m_sigmaCoord);
 
-                    // color weight
-                    Float3 Ci = frameInfo.m_beauty(x, y);
-                    Float3 Cj = frameInfo.m_beauty(i, j);
+//                     // color weight
+//                     Float3 Ci = frameInfo.m_beauty(x, y);
+//                     Float3 Cj = frameInfo.m_beauty(i, j);
 
-                    wc = SqrDistance(Ci, Cj);
-                    wc = wc / (2.0*m_sigmaColor);
+//                     wc = SqrDistance(Ci, Cj);
+//                     wc = wc / (2.0*m_sigmaColor);
 
-                    // normal weight
-                    Float3 Ni = frameInfo.m_normal(x, y);
-                    Float3 Nj = frameInfo.m_normal(i, j);
+//                     // normal weight
+//                     Float3 Ni = frameInfo.m_normal(x, y);
+//                     Float3 Nj = frameInfo.m_normal(i, j);
 
-                    wn = SafeAcos(Dot(Ni, Nj));
-                    wn = wn*wn / (2.0*m_sigmaNormal);
+//                     wn = SafeAcos(Dot(Ni, Nj));
+//                     wn = wn*wn / (2.0*m_sigmaNormal);
 
-                    // plane weight
-                    Float3 Pi = frameInfo.m_position(x, y);
-                    Float3 Pj = frameInfo.m_position(i, j);
-                    Float3 dP = Pj - Pi;
-                    if (Length(dP) > 0.0) dP = Normalize(dP);
+//                     // plane weight
+//                     Float3 Pi = frameInfo.m_position(x, y);
+//                     Float3 Pj = frameInfo.m_position(i, j);
+//                     Float3 dP = Pj - Pi;
+//                     if (Length(dP) > 0.0) dP = Normalize(dP);
 
-                    wd = Dot(Ni, dP);
-                    wd = wd*wd / (2.0*m_sigmaPlane);
+//                     wd = Dot(Ni, dP);
+//                     wd = wd*wd / (2.0*m_sigmaPlane);
 
-                    w = wp + wc + wn + wd;
-                    w = exp(-w);
+//                     w = wp + wc + wn + wd;
+//                     w = exp(-w);
 
-                    Float3 v = frameInfo.m_beauty(i, j);
-                    value_sum += v * w;
-                    weight_sum+= w;
+//                     Float3 v = frameInfo.m_beauty(i, j);
+//                     value_sum += v * w;
+//                     weight_sum+= w;
 
-                }
+//                 }
                 
-            }
+//             }
         
-            filteredImage(x, y) = value_sum / weight_sum;
+//             filteredImage(x, y) = value_sum / weight_sum;
+//         }
+//     }
+    
+    // À-Trous Wavelet
+    int levels = 3;
+    int h = 1;
+    kernelRadius = 2;
+
+    // initialize cache
+    Buffer2D<Float3> cachedImage = CreateBuffer2D<Float3>(width, height);
+    cachedImage.Copy(frameInfo.m_beauty);
+
+    for (size_t l = 0; l < levels; l++)
+    {
+        #pragma omp parallel for
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                // value and weight
+                Float3 value_sum(0.0);
+                float w, wp, wc, wn, wd;
+                float weight_sum= 0.0;
+
+                // filter on current level
+                for (int i = -kernelRadius; i <= kernelRadius; ++i)
+                {
+                    for (int j = -kernelRadius; j <= kernelRadius; ++j) {
+                        int xx = x + h * i;
+                        int yy = y + h * j;
+
+                        if (xx < 0 || xx >= width || yy < 0 || yy >= height) continue;
+
+                        // weights
+                        // position weight
+                        wp = (xx-x)*(xx-x) + (yy-y)*(yy-y);
+                        wp = wp / (2.0*m_sigmaCoord);
+
+                        // color weight
+                        Float3 Ci = cachedImage(x, y);
+                        Float3 Cj = cachedImage(xx, yy);
+
+                        wc = SqrDistance(Ci, Cj);
+                        wc = wc / (2.0*m_sigmaColor);
+
+                        // normal weight
+                        Float3 Ni = frameInfo.m_normal(x, y);
+                        Float3 Nj = frameInfo.m_normal(xx, yy);
+
+                        wn = SafeAcos(Dot(Ni, Nj));
+                        wn = wn*wn / (2.0*m_sigmaNormal);
+
+                        // plane weight
+                        Float3 Pi = frameInfo.m_position(x, y);
+                        Float3 Pj = frameInfo.m_position(xx, yy);
+                        Float3 dP = Pj - Pi;
+                        if (Length(dP) > 0.0) dP = Normalize(dP);
+
+                        wd = Dot(Ni, dP);
+                        wd = wd*wd / (2.0*m_sigmaPlane);
+
+                        w = wp + wc + wn + wd;
+                        w = exp(-w);
+
+                        Float3 v = cachedImage(i, j);
+                        value_sum += v * w;
+                        weight_sum+= w;
+                    }
+                }
+
+                filteredImage(x, y) = value_sum / weight_sum;
+            }
         }
+
+        // update step length
+        h *= 2;
+
+        // update cache
+        cachedImage.Copy(filteredImage);
     }
+    
+    
     return filteredImage;
 }
 
